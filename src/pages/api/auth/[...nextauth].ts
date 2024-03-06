2bc9aa30d1d2099ea22c1b2ce8bdeb078547cb3b
reserver7 <reserver7@gmail.com>
2024-03-06 15:54:42 +0900

    feat: 맛집 목록 구현 (검색 & 정렬)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,6 +10,11 @@ import { Adapter } from "next-auth/adapters";
 const prisma = new PrismaClient();
 
 export const authOptions = {
+  session: {
+    strategy: "jwt" as const,
+    maxAge: 60 * 60 * 24,
+    updateAge: 60 * 60 * 2,
+  },
   adapter: PrismaAdapter(prisma) as Adapter,
   // Configure one or more authentication providers
   providers: [
