8cd7c2afea89295fb6a12b66af88fc26b25274c4
reserver7 <reserver7@gmail.com>
2024-03-13 10:07:53 +0900

    feat: daum 주소 검색 api

--- src/pages/stores/new.tsx
+++ src/pages/stores/new.tsx
@@ -3,23 +3,24 @@ import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { useRouter } from "next/router";
+import AddressSearch from "@/components/AddressSearch";
+import { StoreType } from "@/interface";
 
 export default function StoreNewPage() {
   const router = useRouter();
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
-  } = useForm();
+  } = useForm<StoreType>();
 
   return (
     <form
       className="px-4 md:max-w-4xl mx-auto py-8"
       onSubmit={handleSubmit(async (data) => {
-        console.log(data);
         try {
           const result = await axios.post("/api/stores", data);
-          console.log(result);
 
           if (result.status === 200) {
             // 성공
@@ -30,7 +31,6 @@ export default function StoreNewPage() {
             toast.error("다시 시도해주세요.");
           }
         } catch (e) {
-          console.log(e);
           toast.error("데이터 생성중 문제가 생겼습니다. 다시 시도해주세요.");
         }
       })}
@@ -114,23 +114,11 @@ export default function StoreNewPage() {
             </div>
 
             <div className="col-span-full">
-              <label
-                htmlFor="address"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                주소 (다음 주소 검색 API)
-              </label>
-              <div className="mt-2">
-                <input
-                  {...register("address", { required: true })}
-                  className="block w-full rounded-md border-0 px-2 outline-none py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-                {errors?.address?.type === "required" && (
-                  <div className="pt-2 text-xs text-red-600">
-                    필수 입력사항입니다.
-                  </div>
-                )}
-              </div>
+              <AddressSearch
+                setValue={setValue}
+                register={register}
+                errors={errors}
+              />
             </div>
 
             <div className="sm:col-span-2 sm:col-start-1">
@@ -172,7 +160,7 @@ export default function StoreNewPage() {
                   {...register("storeType", { required: true })}
                   className="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 >
-                  <option value="">식품인증구분 선택</option>
+                  <option value="">업종구분</option>
                   {STORE_TYPE_ARR?.map((data) => (
                     <option key={data} value={data}>
                       {data}
