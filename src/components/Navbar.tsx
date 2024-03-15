e9b43a1f0ce7c717e5496004481d3c6d47de676f
reserver7 <reserver7@gmail.com>
2024-03-15 10:37:19 +0900

    feat: 마이페이지 컴포넌트 작업

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import { useSession, signOut } from "next-auth/react";
 
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
-import React from "react";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,7 +13,7 @@ export default function Navbar() {
     <>
       <div className="navbar">
         <Link className="navbar__logo" href="/">
-          Nextmap
+          nextmap
         </Link>
         <div className="navbar__list">
           <Link href="/stores" className="navbar__list--item">
@@ -25,6 +24,9 @@ export default function Navbar() {
           </Link>
           <Link href="/users/likes" className="navbar__list--item">
             찜한 가게
+          </Link>
+          <Link href="/users/mypage" className="navbar__list--item">
+            마이페이지
           </Link>
           {status === "authenticated" ? (
             <button type="button" onClick={() => signOut()}>
@@ -49,21 +51,54 @@ export default function Navbar() {
       {isOpen && (
         <div className="navbar--mobile">
           <div className="navbar__list--mobile">
-            <Link href="/stores" className="navbar__list--item--mobile">
+            <Link
+              href="/stores"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               맛집 목록
             </Link>
-            <Link href="/stores/new" className="navbar__list--item--mobile">
+            <Link
+              href="/stores/new"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               맛집 등록
             </Link>
-            <Link href="/users/likes" className="navbar__list--item--mobile">
+            <Link
+              href="/users/likes"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               찜한 가게
             </Link>
             <Link
-              href="/api/auth/signin"
+              href="/users/mypage"
               className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
             >
-              로그인
+              마이페이지
             </Link>
+            {status === "authenticated" ? (
+              <button
+                type="button"
+                onClick={() => {
+                  signOut();
+                  setIsOpen(false);
+                }}
+                className="navbar__list--item--mobile text-left"
+              >
+                로그아웃
+              </button>
+            ) : (
+              <Link
+                href="/api/auth/signin"
+                className="navbar__list--item--mobile"
+                onClick={() => setIsOpen(false)}
+              >
+                로그인
+              </Link>
+            )}
           </div>
         </div>
       )}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Markers from "@/components/Markers";
 
 import StoreBox from "@/components/StoreBox";
 import { StoreType } from "@/interface";
+
 import axios from "axios";
 
 export default function Home({ stores }: { stores: StoreType[] }) {
@@ -17,11 +18,10 @@ export default function Home({ stores }: { stores: StoreType[] }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
 
   return {
     props: { stores: stores.data },
-    revalidate: 60 * 60,
   };
 }
