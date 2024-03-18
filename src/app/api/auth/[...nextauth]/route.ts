565b69338f6511d5226e470ffdbb0ffa1e39b774
reserver7 <reserver7@gmail.com>
2024-03-18 15:28:43 +0900

    fix: 오타 수정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,4 @@ import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOptions };
