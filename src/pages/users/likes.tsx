854bf4f5fb4b02695feffbab55962c47d3fd8388
reserver7 <reserver7@gmail.com>
2024-03-18 09:55:08 +0900

    마이페이지 작업

--- src/pages/users/likes.tsx
+++ src/pages/users/likes.tsx
@@ -42,13 +42,12 @@ export default function LikesPage() {
           ))
         )}
       </ul>
-      {likes?.totalPage && likes?.totalPage > 0 && (
-        <Pagination
-          total={likes?.totalPage}
-          page={page}
-          pathname="/users/likes"
-        />
-      )}
+
+      <Pagination
+        total={likes?.totalPage}
+        page={page}
+        pathname="/users/likes"
+      />
     </div>
   );
 }
